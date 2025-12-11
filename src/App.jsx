1d973333dcdbe697a5bc3a8b212f683960b5e201
LakshaniMadhusha <lakshanimadhusha@gmail.com>
2025-12-11 19:47:44 +0530

    Initial project setup(Frontend)📁⚡

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import ProductCard from './components/productCard.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <ProductCard/>
+        <ProductCard/>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/components/productCard.jsx
@@ -0,0 +1,10 @@
+export default function ProductCard(props){
+    return(
+        <div>
+            <h1>Apple Laptop</h1>
+            <img src="https://picsum.photos/id/237/200/300" alt="" />
+            <p>Price 550/=</p>
+            <button>View more</button>
+        </div>
+    )
+}
