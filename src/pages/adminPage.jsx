4afe15aeaaa4545be526530be4cd8f1701a67ca0
LakshaniMadhusha <lakshanimadhusha@gmail.com>
2025-12-21 12:42:30 +0530

    Day 12 [Admin Page]📄✨

--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -1,10 +1,21 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
+import { FaBoxArchive } from "react-icons/fa6";
+import { MdShoppingBag } from "react-icons/md";
+import { FaUsers } from "react-icons/fa";
+import { IoSettingsSharp } from "react-icons/io5";
 
 export default function AdminPage(){
     return(
-        <div className="w-full bg-red-700 h-screen flex">
-            <div className="w-[300px] h-full bg-white"></div>
-             <div className="w-[calc(100%-300px)] bg-blue-500 h-full">
+        <div className="w-full  h-screen flex">
+            <div className="w-[300px] h-full flex flex-col items-center">
+                <span className="text-3xl font-bold my-5">Admin Panel</span>
+                {/* <a href="/admin/products">products</a> */}
+                <Link className="flex flex-row h-[30px] w-full  p-[25px] items-center text-2xl gap-[25px] text-blue-800" to="/admin/products"><FaBoxArchive />Products</Link>
+                <Link className="flex flex-row h-[30px] w-full  p-[25px] items-center text-2xl gap-[25px] text-blue-800" to="/admin/orders"><MdShoppingBag />Orders</Link>
+                <Link className="flex flex-row h-[30px] w-full  p-[25px] items-center text-2xl gap-[25px] text-blue-800" to="/admin/users"><FaUsers />Users</Link>
+                <Link className="flex flex-row h-[30px] w-full  p-[25px] items-center text-2xl gap-[25px] text-blue-800" to="/admin/settings"><IoSettingsSharp />Settings</Link>
+            </div>
+             <div className="w-[calc(100%-300px)]  h-full">
                 <Routes path="/*" >
                        <Route path="/" element={<h1>Dashboard</h1>}/>
                        <Route path="/products" element={<h1>Product</h1>}/>
